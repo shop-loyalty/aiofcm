a15f7144389392be84ebeeaf9ba30b46055d554d
Dmitry Sobolev <ds@napoleonit.ru>
2019-01-18 15:27:41 +0500

    * FCMEnvType moved to package's top level

--- aiofcm/__init__.py
+++ aiofcm/__init__.py
@@ -1,5 +1,6 @@
 from aiofcm.client import FCM
 from aiofcm.common import Message, PRIORITY_NORMAL, PRIORITY_HIGH
+from aiofcm.connection import FCMEnvType
 
 
-__all__ = ['FCM', 'Message', 'PRIORITY_NORMAL', 'PRIORITY_HIGH']
+__all__ = ['FCM', 'Message', 'PRIORITY_NORMAL', 'PRIORITY_HIGH', 'FCMEnvType']
